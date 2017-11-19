4a66c3a28242d8a0c3c9a02b6824c6052dfb1e84
squallee <lishuo78706@gmail.com>
2017-11-19 17:04:01 -0600

    ip change

--- service/main.go
+++ service/main.go
@@ -31,7 +31,7 @@ const (
 	PROJECT_ID = "temporal-sweep-185822"
 	BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://104.196.45.147:9200/"
+	ES_URL = "http://35.196.53.14:9200/"
 
 )
 
